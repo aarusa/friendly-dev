9c9f7a7b0e2d4aa6c0da810202ead9a07ee6830f
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-22 16:38:25 +1000

    Initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "The Friendly Dev" },
+    { name: "description", content: "Custom Website Development" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>
+    My App
+  </>;
 }
