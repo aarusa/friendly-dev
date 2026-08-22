0d1afb261bf1bc0fcadb620f5812878cacad5be3
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-22 17:03:39 +1000

    Adding pages and routes

---
+++ app/routes/about/index.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return ( 
+        <section>
+            <h2 className="text-3xl font-bold">
+                Hey, I'm Arusha 👋
+            </h2>
+        </section>
+     );
+}
+ 
+export default AboutPage;
