836def47745b806ca4d1033d8e597707066ddcca
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-24 13:51:01 +1000

    Creating home layout

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,9 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
-    index('routes/home/index.tsx'),
+    layout('./routes/layouts/home.tsx', [
+        index('routes/home/index.tsx'),
+    ]),
     route('about', './routes/about/index.tsx'),
     route('contact', './routes/contact/index.tsx'),
     route('projects', './routes/projects/index.tsx'),
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,15 +1,12 @@
 import type { Route } from "./+types/index";
-import Hero from "~/components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "The Friendly Dev | Welcome" },
-    { name: "description", content: "Custom Website Development" },
+    { title: "Arusha Portfolio" },
+    { name: "description", content: "Arusha Shahi Portfolio" },
   ];
 }
 
 export default function Home() {
-  return <section>
-    <Hero name="Arusha" />
-  </section>;
+  return <section>Homepage</section>;
 }
