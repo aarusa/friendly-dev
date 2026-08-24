d4adee0790a8a6fe031e7bc9565beed81bfff287
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-24 13:54:40 +1000

    Creating main layout

--- app/routes.ts
+++ app/routes.ts
@@ -4,8 +4,10 @@ export default [
     layout('./routes/layouts/home.tsx', [
         index('routes/home/index.tsx'),
     ]),
-    route('about', './routes/about/index.tsx'),
-    route('contact', './routes/contact/index.tsx'),
-    route('projects', './routes/projects/index.tsx'),
-    route('blog', './routes/blog/index.tsx')
+    layout('./routes/layouts/main.tsx', [
+        route('about', './routes/about/index.tsx'),
+        route('contact', './routes/contact/index.tsx'),
+        route('projects', './routes/projects/index.tsx'),
+        route('blog', './routes/blog/index.tsx')
+    ]),
 ] satisfies RouteConfig;
