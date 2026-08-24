ecfa5caf3f8cca90a5fc1f8c6f445105033d0693
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-24 13:55:48 +1000

    Removed section tags in each pages

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,8 @@
 const ContactPage = () => {
     return ( 
-        <section>
+        <>
             <h2 className="text-3xl font-bold mb-8 text-white text-center">📬 Contact me</h2>
-        </section>
+        </>
      );
 }
  
