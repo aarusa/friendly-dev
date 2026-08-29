699aa2f8c3c86dbe16a0a656a64762801fc309a8
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-29 14:06:53 +1000

    Created project page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,9 +1,11 @@
 import type { Route } from "./+types/index";
 import type { Project } from '~/types';
+import ProjectCard from "~/components/ProjectCard";
 
 export async function loader({request}:Route.LoaderArgs):Promise<{ projects: Project[] }> {
     const res = await fetch('http://localhost:8000/projects');
     const data = await res.json();
+    // console.log(data);
 
     return {projects: data};
 }
@@ -12,11 +14,16 @@ const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
 
     const { projects } = loaderData as { projects: Project[] };
 
-    console.log(projects);
+    // console.log(projects);
     
     return ( 
         <>
             <h2 className="text-3xl font-bold text-white mb-8">🚀 Projects</h2>
+            <div className="grid gap-6 sm:grid-cols-2">
+                {projects.map((project) => (
+                    <ProjectCard key={project.id} project={project} />
+                ))}
+            </div>
         </>
      );
 }
