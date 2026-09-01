9cc0ccb161fc3e00ca47ef70b35a14b5ca0bd6dd
Arusha <admin@Arushas-Macbook.local>
2026-09-01 23:24:43 +1000

    Description added

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -25,7 +25,15 @@ const ProjectDetailsPage = ({ loaderData }:Route.ComponentProps) => {
             </Link>
 
             <div className="grid gap-8 md:grid-cols-2 items-start">
-                <img src={project.image} alt={project.title} className="w-full rounded-lg shadow-md"/>
+                <div>
+                    <img src={project.image} alt={project.title} className="w-full rounded-lg shadow-md"/>
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold text-blue-400 mb-4">{project.title}</h1>
+                    <p className="text-gray-300 text-sm mb-4">{ new Date(project.date).toLocaleDateString() } • {project.category}</p>
+                    <p className="text-gray-200 mb-6">{project.description}</p>
+                    <a href={project.url} target="_blank" className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition">View Live Site →</a>
+                </div>
             </div>
         </>
      );
